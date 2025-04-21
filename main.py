80852d9ced20dcdab31febf13f7169fea2423f68
Involuntar <ilya.nevolin.2004@mail.ru>
2025-04-21 14:03:08 +0500

    сделал вывод и добавление жанров

--- main.py
+++ main.py
@@ -49,41 +49,18 @@ def delete_movie(id:int, db:Session=Depends(get_db)):
     db.commit()
     return {'detail': "Фильм удалён"}
 
-# # Планеты
-# @app.get("/planets", response_model=List[pyd.BasePlanet])
-# def get_all_planets(db:Session=Depends(get_db)):
-#     planets = db.query(m.Planet).all()
-#     return planets
+# Жанры
+@app.get('/genres', response_model=List[pyd.BaseGenre])
+def get_all_genres(db:Session=Depends(get_db)):
+    genres = db.query(m.Genre).all()
+    return genres
 
-# @app.get("/planet/{planet_id}", response_model=pyd.BasePlanet)
-# def get_planet(planet_id:int, db:Session=Depends(get_db)):
-#     planet = db.query(m.Planet).filter(
-#         m.Planet.id==planet_id
-#     ).first()
-#     if not planet:
-#         raise HTTPException(404, 'Планета не найдена')
-#     return planet
+@app.post("/genres", response_model=pyd.CreateGenre)
+def create_genre(genre:pyd.CreateGenre, db:Session=Depends(get_db)):
+    genre_db = m.Genre()
+    genre_db.genre_name = genre.genre_name
+    genre_db.genre_description = genre.genre_description
 
-# @app.post("/planet", response_model=pyd.BasePlanet)
-# def create_planet(planet:pyd.CreatePlanet, db:Session=Depends(get_db)):
-#     planet_db = m.Planet()
-
-#     planet_db.planet_name = planet.planet_name
-#     planet_db.planet_mass = planet.planet_mass
-#     planet_db.planet_diameter = planet.planet_diameter
-
-#     db.add(planet_db)
-#     db.commit()
-#     return planet_db
-
-# @app.delete("/planet/{planet_id}")
-# def delete_product(planet_id:int, db:Session=Depends(get_db)):
-#     planet = db.query(m.Planet).filter(
-#         m.Planet.id==planet_id
-#     ).first()
-#     if not planet:
-#         raise HTTPException(404, 'Планета не найдена')
-#     db.delete(planet)
-#     db.commit()
-#     return {'detail': "Планета удалена"}
-#
+    db.add(genre_db)
+    db.commit()
+    return genre_db
